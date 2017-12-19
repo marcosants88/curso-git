c63241906470e3c88ae9fdfb3c0f9d648bc139aa
Marcos Antonio <marcosants88@gmail.com>
2017-12-19 20:54:10 -0200

    Acrescentado a pergunta How are you know?

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace teste
 		{
 			Console.WriteLine ("Hello World!");
             Console.WriteLine("Good Morning!");
+            Console.WriteLine("How are you know?");
             Console.ReadLine ();
 		}
 	}
